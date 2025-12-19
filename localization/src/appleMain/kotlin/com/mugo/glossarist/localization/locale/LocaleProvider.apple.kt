08544258dcf736c336ec993fe9ecfbcf88e91799
Mugo Lazarus <mugolazarusk@gmail.com>
2025-12-20 01:36:24 +0300

    fix: release
    Updates the workflow to have build before bump

--- localization/src/appleMain/kotlin/com/mugo/glossarist/localization/locale/LocaleProvider.apple.kt
+++
@@ -1,9 +0,0 @@
-package com.mugo.glossarist.localization.locale
-
-import platform.Foundation.NSLocale
-import platform.Foundation.currentLocale
-import platform.Foundation.languageCode
-
-actual class PlatformLocaleProvider actual constructor() : LocaleProvider {
-    actual override fun getCurrentLocale(): String = NSLocale.currentLocale.languageCode
-}
